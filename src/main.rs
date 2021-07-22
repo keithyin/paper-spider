a08e98bd0724f430e55d31493ed922a893d9e1bb
yinpenghhz@hotmail.com <yinpenghhz@hotmail.com>
2021-07-22 11:30:53 +0800

    gg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+use reqwest;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error>{
     println!("Hello, world!");
+    let body = reqwest::get("https://www.baidu.com/").await?.text().await?;
+    println!("{}", body);
+    Ok(())
 }
